fb3bf0402332dc325bfbeb512986d1a80ea38392
varadNVS <101729662+varadNVS@users.noreply.github.com>
2025-11-04 15:15:06 +0530

    Updated main
    updated main to test the addition of the two functions simultaneously.

--- main.cpp
+++ main.cpp
@@ -10,21 +10,14 @@ int main()
 
     for (int i = 0; i < 10; i++)
     {
+        cout << i << endl;
         myList.addData(i);
-        
-    }
-
-    for (int i = 0; i < 10; i++)
-    {
         myList.addDataFront(i * 10);
     }
-    
-
     cout << "is the list empty now: ? " << myList.isEmpty() << endl;
     cout << "------print------" << endl;
     myList.Display();
 
     cout << "position of data 20: " << myList.SearchData(20) << endl;
-
     return 0;
 }
