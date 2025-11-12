016bb3521881ad145d92c47f9f8f76fdd452151d
varadNVS <101729662+varadNVS@users.noreply.github.com>
2025-11-12 16:51:40 +0530

    Added new Test Case.
    Added new Clear operation for Test case.

--- main.cpp
+++ main.cpp
@@ -14,13 +14,17 @@ int main()
     }
     std::cout << "is the list empty now: ? " << myList.isEmpty() << std::endl;
     std::cout << "------print------" << std::endl;
-    myList.RemovalForPos(7);
-    myList.RemovalForData(18);
+    myList.RemovalForPos(1);
+    myList.RemovalForData(13);
     myList.Display();
-    std::cout << myList.SearchData(14) << std::endl;
-    std::cout << "data for 17 :" << myList.SearchPosition(17) << std::endl;
+    std::cout << "data position for 14: " << myList.SearchData(14) << std::endl;
+    std::cout << "data on Pos 17 :" << myList.SearchPosition(17) << std::endl;
+
+
     
-    
-    myList.DisplayHeadAndTail();
+    std::cout << "Clear Check:---------------------------- " << std::endl;
+    myList.ClearAll();
+    myList.Display();
+    std::cout << "code Completed without Crash" << std::endl;
     return 0;
 }
