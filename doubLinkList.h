4964d8cab19326c7f0f0658310a768617442ffa6
varadNVS <101729662+varadNVS@users.noreply.github.com>
2025-11-14 16:05:03 +0530

    Tweaks and Node Shift
    Little Tweaks made for interactive usage and shifted node class to another file

--- doubLinkList.h
+++ doubLinkList.h
@@ -6,59 +6,8 @@
 #define DOUBLE_LINKED_LIST_H
 
 #include <iostream>
-
-class node{
-    private:
-        int data;
-        node *next;
-        node *prev;
-    public:
-        node(int data, node *next, node *prev){
-            this->data = data;
-            this->next = next;
-            this->prev = prev;
-        }
-
-        ~node(){
-            delete next;
-            delete prev;
-            next = nullptr;
-            prev = nullptr;
-        }
-        
-
-        int getData(){
-            return data;
-        }
-        void setData(int data){
-            this->data = data;
-        }
-
-
-        node* getNext(){
-            return next;
-        }
-
-        node* setNext(node* next){
-            this->next = next;
-            return 0;
-        }
-        
-
-        node* getPrevious(){
-            return prev;
-        }
-        node* setPrevious(node* prev){
-            this->prev = prev;
-            return 0;
-        }
-        node* DeleteNode(node* DeletableNodeAddress){
-            DeletableNodeAddress->next = nullptr;
-            DeletableNodeAddress->prev = nullptr;
-            return 0;
-        }
-
-};
+#include <string>
+#include "NodeStruct.h"
 
 
 class doubLinkList
@@ -69,13 +18,21 @@ private:
     node* dllTail;
 public:
 
+    void Printfunc( std::string string){
+        std::cout << string << std::endl;
+    }
+    void Printfunc( std::string string,int a){
+        std::cout << string << a << std::endl;
+    }
     doubLinkList(){
         dllDataNumber = 0;
         dllHead = nullptr;
         dllTail = nullptr;
     }
 
-
+    int getSize(){
+        return dllDataNumber;
+    }
     void addData(int dataAdded,bool direction){
         if (isEmpty())
         {
@@ -101,7 +58,6 @@ public:
                 
             }
             dllDataNumber++;
-            std::cout << "dlldataNumber: " << dllDataNumber << std::endl;
         } 
     }
     void addDataBack(int dataAdded){
